91db7ff99180ec2a57f77d7ea5e9af0d2b0c6418
Arbitr-crypto <olsq4701@gmail.com>
2025-11-27 22:14:18 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# Старт бота
-python3 bot.py
-chmod +x start.sh
+# активируем виртуальное окружение
+python3 -m venv /opt/venv
+source /opt/venv/bin/activate
+
+# устанавливаем зависимости
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# запускаем бота
+python bot.py
